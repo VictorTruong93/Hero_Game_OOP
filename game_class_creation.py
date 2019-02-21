d433036930d588d2f44eda09f7bc3c85216238fd
victor truong <2truong93@gmail.com>
2019-02-21 09:44:28 -0500

    adds Hero and Goblin class

---
+++ game_class_creation.py
@@ -0,0 +1,19 @@
+
+
+
+class Character(object):
+    def __init__(self):
+        self.Health = 10
+        self.Power = 1
+
+
+
+class Hero(Character):
+    def __init__(self):
+        self.Health = 20
+        self.Power = 4
+
+class Goblin(Character):
+    def __init__(self):
+        self.Health= 20
+        self.Power= 4
